3a7d18fb630ed9dc26f2412136a3b3e256b6b86f
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-12-17 17:39:13 -0500

    prettier(?) way to make entry point / root doc url templates

--- app/representers/api/api_representer.rb
+++ app/representers/api/api_representer.rb
@@ -8,7 +8,24 @@ class Api::ApiRepresenter < Roar::Decorator
   end
 
   links :stories do
-    [ { profile: 'http://meta.prx.org/model/story', href: api_stories_path(api_version: represented.version)},
-      { href: api_story_path(represented.version, 632).sub('632', '{id}'), profile: 'http://meta.prx.org/model/story' } ]
+    [
+      { profile: 'http://meta.prx.org/model/story', href: api_stories_path_template(api_version: represented.version)},
+      { profile: 'http://meta.prx.org/model/story', href: api_story_path_template(api_version: represented.version, id: '{id}')}
+    ]
   end
+
+  def method_missing(method_name, *args, &block)
+    if method_name.to_s.ends_with?('_path_template')
+      original_method_name = method_name[0..-10]
+      template_named_path(original_method_name, *args)
+    end
+  end
+
+  def template_named_path(named_path, options)
+    replace_options = options.keys.inject({}){|s,k| s[k] = "#{k.upcase}_REPLACE"; s}
+    path = self.send(named_path, replace_options)
+    replace_options.keys.each{|k| path.gsub!(replace_options[k], options[k])}
+    path
+  end
+
 end
