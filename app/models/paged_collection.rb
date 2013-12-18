94adb170394b9ebd49f2deb14d5c1f3386c97e23
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-12-18 18:06:54 -0500

    use Forwardable to delegate method calls

--- app/models/paged_collection.rb
+++ app/models/paged_collection.rb
@@ -1,42 +1,19 @@
+require 'forwardable'
+
 class PagedCollection
+  extend Forwardable
 
   attr_accessor :items, :request
+
+  def_delegators :request, :params
+
+  def_delegators :items, :count, :total_count, :prev_page, :next_page, :total_pages, :first_page?, :last_page?
+
+  alias_method :total, :total_count
 
   def initialize(items, request)
     self.items = items
     self.request = request
-  end
-
-  def params
-    request.params
-  end
-
-  def count
-    items.count
-  end
-
-  def total
-    items.total_count
-  end
-
-  def prev_page
-    items.prev_page
-  end
-
-  def next_page
-    items.next_page
-  end
-
-  def total_pages
-    items.total_pages
-  end
-
-  def first_page?
-    items.first_page?
-  end
-
-  def last_page?
-    items.last_page?
   end
 
 end
