73ac6f3abafdb671f2877df268fc0e5528ad2e95
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-02-11 16:23:40 -0500

    fixed prx3 to have not null promos field

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,7 +2,7 @@ class Story < PRXModel
 
   self.table_name = 'pieces'
 
-  has_many :audio_versions, -> { where 'promos is null or promos = 0' }
+  has_many :audio_versions, -> { where(promos: false) }
   has_many :audio_files, through: :audio_versions
 
   acts_as_paranoid
