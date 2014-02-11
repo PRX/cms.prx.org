5fc10b4a4f34f35674c11d490b5cfcbc212544b2
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-02-11 09:56:37 -0500

    improved tests for representers

--- test/representers/api/api_representer_test.rb
+++ test/representers/api/api_representer_test.rb
@@ -2,11 +2,29 @@ require "test_helper"
 
 describe Api::ApiRepresenter do
 
-  it "create json for an api with a version" do
-    api = Api.version('1.0')
-    api_representer = Api::ApiRepresenter.new(api)
-    api_representer.wont_be_nil
-    api_representer
+  before(:each) {
+    @api = Api.version('1.0')
+    @api_representer = Api::ApiRepresenter.new(@api)
+  }
+
+  it "create api representer" do
+    @api_representer.wont_be_nil
+  end
+
+  it "use api representer to create json" do
+    @api_representer.to_json.must_equal '{"version":"1.0","_links":{"self":{"href":"/api/1.0"},"stories":[{"profile":"http://meta.prx.org/model/story","href":"/api/1.0/stories{?page}","templated":true},{"profile":"http://meta.prx.org/model/story","href":"/api/1.0/stories/{id}","templated":true}]}}'
+  end
+
+  it "create json for an api version" do
+    h = @api_representer.to_hash
+    h.keys.sort.must_equal ['_links', 'version']
+    h['version'].must_equal '1.0'
+  end
+
+  it "return root doc with links for an api version" do
+    h = @api_representer.to_hash
+    h['_links']['self'][:href].must_equal '/api/1.0'
+    h['_links']['stories'].size.must_equal 2
   end
 
 end
--- test/representers/api/paged_collection_representer_test.rb
+++ test/representers/api/paged_collection_representer_test.rb
@@ -1,5 +1,5 @@
 require "test_helper"
-require 'ostruct'
+require 'paged_collection'
 
 TestObject = Struct.new(:title)
 
@@ -8,26 +8,42 @@ class Api::TestRepresenter < Roar::Decorator
 
   property :title
 
+  def api_tests_path(represented)
+    "/api/tests/#{represented.title}"
+  end
+
   link :self do
-    "/test/#{represented.title}"
+    api_tests_path(represented)
   end
 end
 
 class Api::TestsRepresenter < Roar::Decorator
   include Api::PagedCollectionRepresenter
 
+  def api_tests_path(represented)
+    "/api/tests?page=#{represented[:page] || 1}"
+  end
+
   collection :items, as: :tests, embedded: true, class: TestObject, decorator: Api::TestRepresenter
 end
 
 describe Api::PagedCollectionRepresenter do
 
-  it "create paged collection" do
-    items = (0..25).collect{|t| TestObject.new(title: "test #{t}") }
-    paged_items = Kaminari.paginate_array(items).page(1).per(10)
-    paged_collection = PagedCollection.new(items, OpenStruct.new(params: {}))
-    tests_representer = Api::TestsRepresenter.new(paged_collection)
+  before {
+    @items = (0..25).collect{|t| TestObject.new("test #{t}") }
+    @paged_items = Kaminari.paginate_array(@items).page(1).per(10)
+    @paged_collection = PagedCollection.new(@paged_items, OpenStruct.new(params: {}))
+    @paged_collection_representer = Api::TestsRepresenter.new(@paged_collection)
+  }
 
-    tests_representer.wont_be_nil
+  it "create paged collection" do
+    @paged_collection_representer.wont_be_nil
+  end
+
+  it "paged collection contains tests _links" do
+    h = ActiveSupport::HashWithIndifferentAccess.new(@paged_collection_representer.to_hash)
+    h['_embedded']['tests'].wont_be_nil
+    h['_embedded']['tests'].size.must_equal 10
   end
 
 end
