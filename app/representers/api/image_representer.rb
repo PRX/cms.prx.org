1f062c023c0f9d04827ae0952141cc072a664d7a
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-02-24 12:06:03 -0500

    move media urls to links as enclosures

--- app/representers/api/image_representer.rb
+++ app/representers/api/image_representer.rb
@@ -4,7 +4,13 @@ class Api::ImageRepresenter < Roar::Decorator
   include Roar::Representer::JSON::HAL
 
   property :id
-  property :url
+
+  link :enclosure do
+    {
+      href: represented.url,
+      type: represented.content_type || 'image'
+    }
+  end
 
   link :self do 
     polymorphic_path([:api, represented])
