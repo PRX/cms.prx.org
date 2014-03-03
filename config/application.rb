c89097e2cc824110c8e5d49fe1fff020fe43319b
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-03-02 22:57:08 -0500

    add representer concerns to the app load path

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module PRX
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.autoload_paths += %W( #{config.root}/app/representers/concerns )
+
     # Disable the asset pipeline.
     config.assets.enabled = false
 
