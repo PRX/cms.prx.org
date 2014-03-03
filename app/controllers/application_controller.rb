cca7609bfe715bb733aab83fbd073e69f56a8e6a
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-03-02 23:16:37 -0500

    404 on record not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  def record_not_found
+    render text: "404 Not Found\n", status: 404
+  end
+
 end
