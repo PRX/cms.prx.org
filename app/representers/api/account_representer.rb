73f80433f42e739e2ec398998bb730af88b0d160
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-03-27 15:47:05 -0400

    represent accounts properly

--- app/representers/api/account_representer.rb
+++ app/representers/api/account_representer.rb
@@ -7,6 +7,11 @@ class Api::AccountRepresenter < Api::BaseRepresenter
   property :name
   property :path
 
+  link :address do
+    api_address_path(represented.address) if represented.address
+  end
+  embed :address, class: Address, decorator: Api::AddressRepresenter, zoom: true
+
   link :image do
     {
       href:    polymorphic_path([:api, represented.image]),
@@ -16,17 +21,17 @@ class Api::AccountRepresenter < Api::BaseRepresenter
   end
   embed :image, class: Image, decorator: Api::ImageRepresenter, zoom: true
   
-  link :address do
-    api_address_path(represented.address) if represented.address
-  end
-  embed :address, class: Address, decorator: Api::AddressRepresenter
-
   link :opener do
     {
       href: api_user_path(represented.opener),
       title: represented.opener.login
     } if represented.opener
   end
-  embed :opener, class: User, decorator: Api::UserRepresenter
+  embed :opener, class: User, decorator: Api::Min::UserRepresenter
+
+  link :stories do
+    api_account_stories_path(represented)
+  end
+  embed :stories, as: :stories, paged: true, item_class: Story, item_decorator: Api::Min::StoryRepresenter, zoom: true
 
 end
