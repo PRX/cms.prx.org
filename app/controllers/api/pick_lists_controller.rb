6a72570ba4f62639d60866e20e3cc2008bc7b31d
Rebecca Nesson <rebecca@prx.org>
2014-05-22 15:23:15 -0400

    removing comment block from pick list controller

--- app/controllers/api/pick_lists_controller.rb
+++ app/controllers/api/pick_lists_controller.rb
@@ -8,10 +8,5 @@ class Api::PickListsController < Api::BaseController
     @pick_list = PickList.find_by_id(params[:id])
   end
 
-# TODO: refactor existing app to have membership record for user to be admin of individual account
-# This would be the right thing to do if there was consistency in how accounts are associated to a user.
-# Once that TODO above is done, this can be used instead of the `resources` method
-  # filter_resources_by :user_id
-
 end
 
