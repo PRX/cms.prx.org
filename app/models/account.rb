5c817f28d8fe10da5502f5d726e32ede8687bd6c
Rebecca Nesson <rebecca@prx.org>
2014-05-30 13:34:13 -0400

    moving short name into subclasses where differnet from account, changing stations to use name as short name

--- app/models/account.rb
+++ app/models/account.rb
@@ -16,13 +16,7 @@ class Account < BaseModel
   scope :member,  -> { where type: ['StationAccount', 'GroupAccount'] }
 
   def short_name
-    if type == 'IndividualAccount'
-        opener.first_name
-    elsif type == 'StationAccount'
-        station_call_letters
-    else
-        name
-    end
+    name
   end
 
 end
--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -12,20 +12,8 @@ describe Account do
     account.stories.count.must_be :>, 0
   end
 
-  it 'has first name as short name if it is an individual account' do
-    user = build_stubbed(:user, {:first_name => 'sigil'})
-    individual = build_stubbed(:account, {:type => 'IndividualAccount', :opener => user})
-    individual.short_name.must_equal 'sigil'
-  end
-
-  it 'has call letters as short name if it is a station account' do
-    station = build_stubbed(:account, {:type => 'StationAccount', :station_call_letters => 'sigil'})
-    station.short_name.must_equal 'sigil'
-  end
-
-  it 'has account name as short name if it s a group account' do
-    group = build_stubbed(:account, {:type => 'GroupAccount', :name => 'sigil'})
-    group.short_name.must_equal 'sigil'
+  it 'has name as short name' do
+    account.short_name.must_equal account.name
   end
 
 end
