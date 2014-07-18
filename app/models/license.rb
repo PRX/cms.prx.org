35eae3f597efb08954ea023d3de6f8b5a66d36ec
Chris Rhoden <carhoden@gmail.com>
2014-07-18 15:28:19 -0400

    Simplify license display to reflect plans for license terms
    Right now we have two sets of radio buttons that really should just be
    two checkboxes. Specifically:
    
    Allow Edit: "only with permission" and "never" seem to essentially mean the
    same thing, so we collapse them.
    
    Allow use on website: downloading as an MP3 doesn't really seem to be a thing
    stations care about offering, and if they do they are always welcome to ask.
    Thus, we can simplify the choice to whether or not streaming on a licensee's
    website is ok.

--- app/models/license.rb
+++ app/models/license.rb
@@ -5,8 +5,16 @@ class License < BaseModel
   WEBSITE_USAGE = ['as a free MP3 download and stream', 'as a stream only', 'only with permission']
   ALLOW_EDIT = ['only with permission', 'without further permission', 'never']
 
-  belongs_to :story, class_name: 'Story', foreign_key: 'piece_id', with_deleted: true 
+  belongs_to :story, class_name: 'Story', foreign_key: 'piece_id', with_deleted: true
 
   acts_as_paranoid
+
+  def streamable
+    WEBSITE_USAGE[0..1].include?(website_usage)
+  end
+
+  def editable
+    ALLOW_EDIT[1] == allow_edit
+  end
 
 end
--- app/representers/api/license_representer.rb
+++ app/representers/api/license_representer.rb
@@ -2,9 +2,7 @@
 
 class Api::LicenseRepresenter < Api::BaseRepresenter
 
-  property :id
-  property :website_usage
-  property :allow_edit
-  property :additional_terms
+  property :streamable
+  property :editable
 
 end
