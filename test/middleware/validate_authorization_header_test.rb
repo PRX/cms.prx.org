d9c06b43ce8b87467870c8482964f76da7aaf918
Eve Asher <eve.adele.asher@gmail.com>
2014-09-26 10:32:59 -0400

    sub JSON::JWT methods in rack middleware test

--- test/middleware/validate_authorization_header_test.rb
+++ test/middleware/validate_authorization_header_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 describe ValidateAuthorizationHeader do
   let(:app) { Proc.new {|env| env } }
   let(:valauth) { ValidateAuthorizationHeader.new(app) }
-  let(:access_token) { 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJzdWIiOm51bGwsImV4cCI6MzYwMCwiaWF0IjoxNDExNjY4NDIyLCJ0b2tlbl90eXBlIjoiYmVhcmVyIiwic2NvcGUiOm51bGx9.MEUCICZEJN1c-oCAteB7nH6QA7hvD6a66jTRi4E2dTb0vNHOAiEA5f9HK04GnsDnaNpA_SdwyW-nrCerb96M4PHLxmHrQ9U' }
-  # TODO: Generate a test access token instead of hardcoding
+  let(:env) { {'HTTP_AUTHORIZATION' => 'Bearer zdawefawefawefw' } }
+  let(:claims) { claims = {'sub'=>nil, 'exp'=>3600, 'iat'=>1411668422, 'token_type'=>'bearer', 'scope'=>nil} }
 
   describe '#call' do
     it 'does nothing if there is no authorization header' do
@@ -14,18 +14,17 @@ describe ValidateAuthorizationHeader do
     end
 
     it 'returns 401 if verification fails' do
-      env = {'HTTP_AUTHORIZATION' => 'Bearer ' + access_token }
+      raise_error = Proc.new { raise JSON::JWT::VerificationFailed }
 
-      valauth.public_key.stub(:key, nil) do
+      JSON::JWT.stub(:decode, raise_error) do
         valauth.call(env).must_equal [401, {'Content-Type' => 'application/json'}, [{status: 401, error: 'Invalid JSON Web Token'}.to_json]]
       end
     end
 
     it 'attaches claims to request params if verification passes' do
-      env = {'HTTP_AUTHORIZATION' => 'Bearer ' + access_token }
-      claims = {'sub'=>nil, 'exp'=>3600, 'iat'=>1411668422, 'token_type'=>'bearer', 'scope'=>nil}
-
-      valauth.call(env)['prx.auth'].must_equal claims
+      JSON::JWT.stub(:decode, claims) do
+        valauth.call(env)['prx.auth'].must_equal claims
+      end
     end
   end
 end
