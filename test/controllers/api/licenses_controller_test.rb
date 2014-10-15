151db173bb4cd45041f3c41b7a73c0704db18bd6
Eve Asher <eve.adele.asher@gmail.com>
2014-10-15 11:48:42 -0400

    remove unnecessary controller tests

--- test/controllers/api/licenses_controller_test.rb
+++ test/controllers/api/licenses_controller_test.rb
@@ -15,23 +15,4 @@ describe Api::LicensesController do
     get(:index, { api_version: 'v1', format: 'json' } )
     assert_response :success
   end
-
-  describe '#update' do
-    it 'should update if user has permission' do
-      @controller.stub(:current_user, user) do
-        get(:update, api_version: 'v1', format: 'json', id: license.id)
-      end
-
-      assert_response :success
-    end
-
-    it 'should not update if user does not have permission' do
-      user2 = create(:user)
-      @controller.stub(:current_user, user2) do
-        get(:update, api_version: 'v1', format: 'json', id: license.id)
-      end
-
-      assert_response :unauthorized
-    end
-  end
 end
