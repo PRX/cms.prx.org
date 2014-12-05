da6b87664ad9c87cad31301f420eef27cd06c7d5
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-12-05 14:48:19 -0500

    add new concern to provide writeable property for links

---
+++ app/representers/concerns/link_serialize.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+
+require 'active_support/concern'
+
+module LinkSerialize
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+
+    def link(options, &block)
+      if options.is_a?(Hash) && (options.delete(:writeable)|| options[:reader])
+        name   = options[:rel].to_s.split(':').last.split('/').last
+        pname  = "set_#{name}_uri"
+        reader = options.delete(:reader) || ->(doc, args){ self.try("#{name}_id=", id_from_url(doc[pname])) if doc[pname] }
+
+        self.property( pname, readable: false, reader: reader)
+      end
+      super(options, &block)
+    end
+
+  end
+
+end
