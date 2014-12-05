5a7e0c58dc0062777c5397ad11a84d008ff360e1
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-12-05 17:50:29 -0500

    cleanup test

--- test/representers/concerns/link_serialize_test.rb
+++ test/representers/concerns/link_serialize_test.rb
@@ -6,10 +6,6 @@ describe LinkSerialize do
 
   it 'sets a default curie' do
 
-    class Foo
-      def self.id_for_url(url); 1; end
-    end
-
     class TestRepresenter < Roar::Decorator
       include Roar::Representer::JSON::HAL
       include LinkSerialize
@@ -17,12 +13,10 @@ describe LinkSerialize do
       link rel: :foo, writeable: true do
         { href: '/foo' }
       end
-
     end
 
     TestRepresenter.representable_attrs['set_foo_uri'].wont_be_nil
     TestRepresenter.representable_attrs['set_foo_uri'][:readable].must_equal false
     TestRepresenter.representable_attrs['set_foo_uri'][:reader].wont_be_nil
   end
-
 end
