3e917b8e88989ea82543c3bae603e956199ebe67
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-12-15 15:25:55 -0500

    update representer for license to have no links

--- app/representers/api/license_representer.rb
+++ app/representers/api/license_representer.rb
@@ -1,6 +1,9 @@
 # encoding: utf-8
 
-class Api::LicenseRepresenter < Api::BaseRepresenter
+class Api::LicenseRepresenter < Roar::Decorator
+
+  include Roar::Representer::JSON::HAL
+  include Caches
 
   property :streamable
   property :editable
