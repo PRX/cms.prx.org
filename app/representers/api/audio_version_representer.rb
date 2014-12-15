01c3b2705f7f79a67de5ba2b2092392ee59eaa88
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-12-15 15:26:47 -0500

    add audio to audio version representer

--- app/representers/api/audio_version_representer.rb
+++ app/representers/api/audio_version_representer.rb
@@ -6,4 +6,9 @@ class Api::AudioVersionRepresenter < Api::BaseRepresenter
   property :label
   property :timing_and_cues
 
+  link :audio do
+    api_audio_version_audio_files_path(represented)
+  end
+  embeds :audio_files, as: :audio, class: AudioFile, decorator: Api::AudioFileRepresenter
+
 end
