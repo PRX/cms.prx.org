a43b8f4012a8a69ec850412e4ec55cd6bd0c7131
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-12-15 16:46:23 -0500

    use helper to get story

--- app/controllers/api/promos_controller.rb
+++ app/controllers/api/promos_controller.rb
@@ -1,21 +1,26 @@
+# encoding: utf-8
+
 class Api::PromosController < Api::BaseController
 
   api_versions :v1
 
   def resource
-    @promo ||= if (params[:story_id] && params[:id])
-      Story.find(params[:story_id]).promos.audio_files.find(params[:id])
+    @promo ||= if story && params[:id]
+      story.promos.audio_files.find(params[:id])
     else
-      Story.find(params[:story_id]).promos.audio_files.build
+      story.promos.audio_files.build
     end
   end
 
   def resources
-    @promos ||= Story.find(params[:story_id]).promos.audio_files.page(params[:page])
+    @promos ||= story.promos.audio_files.page(params[:page])
+  end
+
+  def story
+    @story ||= Story.find(params[:story_id]) if params[:story_id]
   end
 
   def self.resource_class
     AudioFile
   end
-
 end
