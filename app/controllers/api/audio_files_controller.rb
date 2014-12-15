570e2be85ecd138bdaebabd41bcedf742819c624
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-12-15 16:45:00 -0500

    use story id correctly to get audio files

--- app/controllers/api/audio_files_controller.rb
+++ app/controllers/api/audio_files_controller.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class Api::AudioFilesController < Api::BaseController
 
   api_versions :v1
@@ -5,9 +7,11 @@ class Api::AudioFilesController < Api::BaseController
   filter_resources_by :audio_version_id
 
   def resources
-    super
-    # return super unless params[:story_id]
-    # self.resources = Story.find(params[:story_id]).default_audio_version.audio_files.page(params[:page])
+    return super unless story
+    self.resources = story.default_audio_version.audio_files.page(params[:page])
   end
 
+  def story
+    @story ||= Story.find(params[:story_id]) if params[:story_id]
+  end
 end
