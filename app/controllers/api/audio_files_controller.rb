853566c3b903e1218beb7ce40d39562b433d89cb
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-12-16 12:14:39 -0500

    set audio version from story

--- app/controllers/api/audio_files_controller.rb
+++ app/controllers/api/audio_files_controller.rb
@@ -6,6 +6,13 @@ class Api::AudioFilesController < Api::BaseController
 
   filter_resources_by :audio_version_id
 
+  # if the audio version is not set, and story is, set default av
+  def resource
+    r = super
+    r.audio_version_id = story.default_audio_version if story && !r.audio_version_id
+    r
+  end
+
   def resources
     return super unless story
     self.resources = story.default_audio_version.audio_files.page(params[:page])
