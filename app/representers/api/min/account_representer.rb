303e9e599b90ea23a647f76bd73088bb9076ce5e
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-12-16 16:04:51 -0500

    update to match full representer

--- app/representers/api/min/account_representer.rb
+++ app/representers/api/min/account_representer.rb
@@ -8,8 +8,9 @@ class Api::Min::AccountRepresenter < Api::BaseRepresenter
   property :path
   property :short_name
 
+
   link :address do
-    api_address_path(represented.address) if represented.address
+    api_account_address_path(represented.id) if represented.id
   end
   embed :address, class: Address, decorator: Api::AddressRepresenter
 
@@ -31,8 +32,10 @@ class Api::Min::AccountRepresenter < Api::BaseRepresenter
   embed :opener, class: User, decorator: Api::Min::UserRepresenter, zoom: false
 
   link :stories do
-    api_account_stories_path(represented)
+    {
+      href: "#{api_account_stories_path(represented)}{?filters}",
+      templated: true
+    }
   end
   embed :stories, paged: true, item_class: Story, item_decorator: Api::Min::StoryRepresenter, zoom: false
-
 end
