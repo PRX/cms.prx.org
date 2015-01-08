3322856c7f24f881eea1be61681aa25df4152e6e
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-01-07 19:13:49 -0500

    add test for adding paging to resources query

--- test/controllers/concerns/hal_actions_test.rb
+++ test/controllers/concerns/hal_actions_test.rb
@@ -13,10 +13,15 @@ describe HalActions do
   end
 
   let (:controller) { HalActionsTestController.new }
-  let(:account) { create(:account) }
+  let (:account) { create(:account) }
 
   it 'has cache options' do
     HalActionsTestController.cache_options.must_equal({compress: true, expires_in: 1.hour, race_condition_ttl: 30})
+  end
+
+  it 'can adds paging to resources query' do
+    arel = HalActionsTestController.new.with_paging(Account.where('id is not null'))
+    arel.to_sql.must_match /LIMIT 10 OFFSET 0/
   end
 
   describe '#update' do
