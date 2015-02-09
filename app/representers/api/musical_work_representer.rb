dbcabd942fb7e58ad1fcce2f8c9eb29894e93fa8
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-02-09 11:35:51 -0500

    add self_url declaration to musical works, since it is nested under story

--- app/representers/api/musical_work_representer.rb
+++ app/representers/api/musical_work_representer.rb
@@ -10,4 +10,8 @@ class Api::MusicalWorkRepresenter < Api::BaseRepresenter
   property :album
   property :year
   property :excerpt_length, as: :length
+
+  def self_url(musical_work)
+    polymorphic_path([:api, musical_work.story, musical_work])
+  end
 end
