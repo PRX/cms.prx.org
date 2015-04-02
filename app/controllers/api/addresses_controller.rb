c1c21e3b37bf6ef006301248b52d2ec0ad81b6b0
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-04-02 16:53:31 -0400

    address representer should not get user address, should get address by id or by account_id

--- app/controllers/api/addresses_controller.rb
+++ app/controllers/api/addresses_controller.rb
@@ -4,6 +4,14 @@ class Api::AddressesController < Api::BaseController
 
   api_versions :v1
 
-  filter_resources_by :user_id, :account_id
+  filter_resources_by :account_id
 
+  def resource
+    @address = account.address if !super && account
+    @address
+  end
+
+  def account
+    @account ||= Account.find(params[:account_id]) if params[:account_id]
+  end
 end
