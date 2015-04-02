128b206fa5a559e13b9a77a27f08ed56747e3e4c
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-04-02 16:50:25 -0400

    relate account to address

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,4 +4,11 @@ class Address < BaseModel
 
   belongs_to :addressable, polymorphic: true
 
+  def account
+    if addressable.is_a?(Account)
+      addressable
+    elsif addressable.is_a?(User)
+      addressable.individual_account
+    end
+  end
 end
