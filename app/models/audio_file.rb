8d739516c5bf48f557d9be1884e5cd9daaddc80e
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-04-10 18:45:05 -0700

    get signed url for upload if it uses the same storage provider as the audio file uploader

--- app/models/audio_file.rb
+++ app/models/audio_file.rb
@@ -36,10 +36,70 @@ class AudioFile < BaseModel
     end
   end
 
+  # `upload` can be any of the protocols of url used by fixer
+  # http(s), ftp, s3, ia...
   def asset_url(options = {})
     v = options[:version]
     v = nil if (v.blank? || v.to_s == 'original')
-    final_location? ? file.try(:url, *v) : upload
+    final_location? ? file.try(:url, *v) : upload_url
+  end
+
+  def upload_url
+    uri = URI.parse(upload)
+    if uri.scheme.starts_with?('http')
+      upload
+    elsif uri.scheme.starts_with?('ftp')
+      nil
+    elsif AudioFile.storage_providers[uri.scheme.to_s]
+      signed_url(upload)
+    end
+  end
+
+  def self.storage_providers
+    {
+      's3'        => 'AWS',
+      'ia'        => 'InternetArchive',
+      'google'    => 'Google',
+      'rackspace' => 'Rackspace',
+      'openstack' => 'OpenStack'
+    }
+  end
+
+  def self.storage_provider_for_uri(uri)
+    uri = URI.parse(uri.to_s) unless uri.is_a?(URI)
+    storage_providers[uri.scheme]
+  end
+
+  def signed_url(uri = upload, options = {})
+    return if upload.blank?
+
+    uri = URI.parse(uri.to_s) unless uri.is_a?(URI)
+    bucket = uri.host
+    path = uri.path[1..-1]
+    provider = AudioFile.storage_provider_for_uri(uri)
+
+    credentials = AudioFileUploader.fog_credentials
+    connection = Fog::Storage.new(credentials)
+
+    if provider == credentials[:provider]
+      # avoid a get by using local references
+      local_directory = connection.directories.new(key: bucket)
+      local_file = local_directory.files.new(key: path)
+      if credentials[:provider] == 'AWS'
+        local_file.url(url_expires_at(options), options)
+      elsif ['Rackspace', 'OpenStack'].include?(credentials[:provider])
+        connection.get_object_https_url(bucket, path, url_expires_at(options))
+      else
+        local_file.url(url_expires_at(options))
+      end
+    else
+      nil
+    end
+  end
+
+  def url_expires_at(options)
+    ::Fog::Time.now + (options.delete(:expiration) ||
+    AudioFileUploader.fog_authenticated_url_expiration)
   end
 
   def public_asset_filename
--- test/models/audio_file_test.rb
+++ test/models/audio_file_test.rb
@@ -26,4 +26,37 @@ describe AudioFile do
   it 'can have a url from the upload' do
     audio_file_uploaded.filename.must_equal 'test.mp3'
   end
+
+  it 'can have an asset_url from an upload' do
+    audio_file_uploaded.asset_url.must_equal audio_file_uploaded.upload
+  end
+
+  it 'can have a signed url from an upload' do
+    audio_file_uploaded.upload = 's3://prx-development/another.mp3'
+    asset_url = audio_file_uploaded.asset_url
+    asset_url.must_match /another.mp3/
+    asset_url.must_match /X-Amz-Expires/
+    asset_url.must_match /X-Amz-Credential/
+  end
+
+  it 'can get a provider for a url scheme' do
+    AudioFile.storage_providers['s3'].must_equal 'AWS'
+  end
+
+  it 'can get the storage provider for a uri' do
+    AudioFile.storage_provider_for_uri('google://g.com/p/f.mp3').must_equal 'Google'
+  end
+
+  it 'can returned a signed url for a file' do
+    signed_url = audio_file_uploaded.signed_url('s3://prx-development/another.mp3', expiration: 3600)
+    signed_url.must_match /another.mp3/
+    signed_url.must_match /X-Amz-Expires=3600/
+  end
+
+  it 'can determine an expiration time' do
+    n = ::Fog::Time.now
+    ::Fog::Time.stub(:now, n) do
+      audio_file_uploaded.url_expires_at(expiration: 3600).must_equal (n + 3600)
+    end
+  end
 end
