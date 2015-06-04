310b8d6b2465c38bc2cd5e5fa0a29db9ae57f3b9
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-06-04 10:44:47 -0400

    shorter line

--- config/initializers/shoryuken.rb
+++ config/initializers/shoryuken.rb
@@ -1,3 +1,4 @@
 require 'shoryuken'
 
-Shoryuken::EnvironmentLoader.load(config_file: (Rails.root + 'config' + 'shoryuken.yml'))
+config_file = File.join(Rails.root, 'config', 'shoryuken.yml')
+Shoryuken::EnvironmentLoader.load(config_file: config_file)
