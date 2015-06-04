2572b68c24f433524992c39df35a99f4e4ae2216
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-06-04 10:00:21 -0400

    remove announce includes, move to concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   include Pundit
-  include Announce::Publisher
   protect_from_forgery with: :exception
 
 end
