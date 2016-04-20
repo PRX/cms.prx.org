84d27844bc99bd32efb60b9f9de8d35a6bebaf6b
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-04-20 11:57:59 -0400

    perhaps paranoid, make sure the account has an id/persisted

--- app/representers/api/auth/account_representer.rb
+++ app/representers/api/auth/account_representer.rb
@@ -1,14 +1,13 @@
 # encoding: utf-8
 
 class Api::Auth::AccountRepresenter < Api::AccountRepresenter
-
   # point to authorized stories (including unpublished)
   link :stories do
     {
       href: "#{api_authorization_account_stories_path(represented)}{?page,per,zoom,filters}",
       templated: true,
       count: represented.all_stories.count
-    }
+    } if represented.id
   end
   embed :all_stories, as: :stories, paged: true, item_class: Story,
                       item_decorator: Api::Auth::StoryMinRepresenter,
@@ -17,5 +16,4 @@ class Api::Auth::AccountRepresenter < Api::AccountRepresenter
   def self_url(r)
     api_authorization_account_path(r)
   end
-
 end
