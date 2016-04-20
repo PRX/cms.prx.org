d11793eef470815faed1b9f0355b931ff14c3ac3
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-04-20 11:14:22 -0400

    mime type config handled by HAR

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,11 +3,3 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
-Mime::Type.register 'application/hal+json', :hal
-
-# roar fix for 4.1
-# https://github.com/apotonick/roar-rails/issues/65
-ActionController.add_renderer :hal do |js, options|
-  self.content_type ||= Mime::HAL
-  js.to_json
-end
