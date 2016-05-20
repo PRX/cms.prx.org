3fbf1856e8e1ad48f32d367b33dce8065f233bc8
cavis <ryancavis@gmail.com>
2016-05-20 08:52:36 -0600

    Back to memcached

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -39,8 +39,8 @@ PRX::Application.configure do
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
-  # Use a different cache store in production.
-  config.cache_store = :memory_store
+  # Use a different cache store in staging.
+  config.cache_store = :mem_cache_store, *ENV['MEMCACHE_SERVERS'].split(',')
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
