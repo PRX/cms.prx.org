61aa6550b2c1b444df4841962679b701154ca360
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-07-12 15:58:56 -0400

    get rid of network_pieces

--- app/models/network.rb
+++ app/models/network.rb
@@ -2,6 +2,5 @@
 
 class Network < BaseModel
   belongs_to :account
-  has_many :network_pieces
-  has_many :pieces, through: :network_pieces
+  has_many :pieces
 end
--- app/models/network_piece.rb
+++
@@ -1,6 +0,0 @@
-# encoding: utf-8
-
-class NetworkPiece < BaseModel
-  belongs_to :piece
-  belongs_to :network
-end
