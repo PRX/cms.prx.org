cc330fda86522ea7b602dffbed4feb3da7db4e78
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-08-24 18:31:17 -0400

    Make tests better, actually check some values

--- test/controllers/api/networks_controller_test.rb
+++ test/controllers/api/networks_controller_test.rb
@@ -1,17 +1,22 @@
 require 'test_helper'
+require 'json'
 
 describe Api::NetworksController do
   let(:user) { create(:user) }
   let(:network) { create(:network, account: user.individual_account) }
 
   it 'should show' do
-    get(:show, { api_version: 'v1', format: 'json', id: network.id } )
+    get(:show, api_version: 'v1', format: 'json', id: network.id)
+    resource = JSON.parse(@response.body)
+    resource["id"].must_equal network.id
     assert_response :success
   end
 
   it 'should list' do
     network.id.wont_be_nil
-    get(:index, { api_version: 'v1', format: 'json' } )
+    get(:index, api_version: 'v1', format: 'json')
+    list = JSON.parse(@response.body)
+    list["total"].must_equal 1
     assert_response :success
   end
 end
