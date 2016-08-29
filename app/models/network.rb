d6d125979627024f828215ad65120a1e55654780
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-08-29 17:39:58 -0400

    Fix foreign key

--- app/models/network.rb
+++ app/models/network.rb
@@ -3,5 +3,5 @@
 class Network < BaseModel
   belongs_to :account
   has_many :stories, -> { where('published_at is not null and network_only_at is null').order(published_at: :desc) }
-  has_many :all_stories, -> { where('published_at is not null').order(published_at: :desc) }, foreign_key: 'account_id', class_name: 'Story'
+  has_many :all_stories, -> { where('published_at is not null').order(published_at: :desc) }, foreign_key: 'network_id', class_name: 'Story'
 end
