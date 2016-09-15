cb54614583bf4126347bcf6bdd70e2b5b969ed5f
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-09-15 11:50:41 -0400

    set the default account properly when membership removed

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,9 +1,16 @@
 # encoding: utf-8
 
 class Membership < BaseModel
-
   belongs_to :user, -> { with_deleted }, touch: true
   belongs_to :account, -> { with_deleted }, touch: true
 
   scope :approved, -> { where(approved: true) }
+
+  after_commit :update_user_default_account, on: [:destroy]
+
+  def update_user_default_account
+    if user.default_account == account
+      user.update_attributes!(account_id: user.individual_account.id)
+    end
+  end
 end
--- test/models/membership_test.rb
+++ test/models/membership_test.rb
@@ -1,11 +1,19 @@
 require 'test_helper'
 
 describe Membership do
-
-  let(:membership) { FactoryGirl.create(:membership) }
+  let(:membership) { create(:membership) }
+  let(:user) { membership.user }
 
   it 'has a table defined' do
     Membership.table_name.must_equal 'memberships'
   end
 
+  it 'sets user default account to individual if current default membership destroyed' do
+    user.default_account = membership.account
+    user.default_account.must_equal membership.account
+    membership.destroy
+    user.reload
+    user.default_account.wont_equal membership.account
+    user.default_account.must_equal user.default_account
+  end
 end
