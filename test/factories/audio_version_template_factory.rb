88c8ea698c5957d56488ae16832f450996419fd0
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-09-16 19:04:58 -0400

    default to 4 files required

--- test/factories/audio_version_template_factory.rb
+++ test/factories/audio_version_template_factory.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :audio_version_template do
     series
     label 'test'
-    segment_count 1
+    segment_count 4
     length_minimum 10
     length_maximum 100
   end
