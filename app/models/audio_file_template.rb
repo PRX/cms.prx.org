4e9658a9104200d043024bee74fb92ed69daf7b6
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-09-16 10:01:36 -0400

    Add in file template validations

--- app/models/audio_file_template.rb
+++ app/models/audio_file_template.rb
@@ -2,4 +2,13 @@
 
 class AudioFileTemplate < BaseModel
   belongs_to :audio_version_template
+  acts_as_list scope: :audio_version_template
+
+  validates :length_minimum,
+    presence: true,
+    numericality: { less_than_or_equal_to: :length_maximum }
+
+  validates :length_maximum,
+    presence: true,
+    numericality: { greater_than_or_equal_to: :length_minimum }
 end
