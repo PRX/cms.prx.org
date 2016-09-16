751dd2ad4bb2cdddfebb29f6f3d31761c5b09d4d
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-09-16 18:55:48 -0400

    add defaults, fix indent

--- app/models/audio_file_template.rb
+++ app/models/audio_file_template.rb
@@ -4,11 +4,19 @@ class AudioFileTemplate < BaseModel
   belongs_to :audio_version_template
   acts_as_list scope: :audio_version_template
 
+  before_validation :set_defaults, on: :create
+
   validates :length_minimum,
-    presence: true,
-    numericality: { less_than_or_equal_to: :length_maximum }
+            presence: true,
+            numericality: { less_than_or_equal_to: :length_maximum }
 
   validates :length_maximum,
-    presence: true,
-    numericality: { greater_than_or_equal_to: :length_minimum }
+            presence: true,
+            numericality: { greater_than_or_equal_to: :length_minimum }
+
+  def set_defaults
+    self.label ||= 'segment'
+    self.length_minimum ||= 0
+    self.length_maximum ||= 0
+  end
 end
--- test/factories/audio_file_template_factory.rb
+++ test/factories/audio_file_template_factory.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :audio_file_template do
     audio_version_template
-    label 'segment'
     length_minimum 1
     length_maximum 10
   end
