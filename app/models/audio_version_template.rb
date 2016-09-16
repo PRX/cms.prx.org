f06d5b3a39cb9f73bfbfd195fa57bcd5fa02fedb
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-09-16 18:58:01 -0400

    handle segment count changes, format validations

--- app/models/audio_version_template.rb
+++ app/models/audio_version_template.rb
@@ -9,21 +9,21 @@ class AudioVersionTemplate < BaseModel
   validates :label, presence: true
 
   validates :length_minimum,
-    presence: true,
-    numericality: { less_than_or_equal_to: :length_maximum }
+            presence: true,
+            numericality: { less_than_or_equal_to: :length_maximum }
 
   validates :length_maximum,
-    presence: true,
-    numericality: { greater_than_or_equal_to: :length_minimum }
+            presence: true,
+            numericality: { greater_than_or_equal_to: :length_minimum }
 
   validates :segment_count,
-    presence: true,
-    numericality: { only_integer: true, greater_than: 0 }
+            presence: true,
+            numericality: { only_integer: true, greater_than: 0 }
 
   def sync_audio_file_templates
     diff = segment_count - audio_file_templates.count
     if diff > 0
-      diff.times { audio_file_templates.create(label: 'segment') }
+      diff.times { audio_file_templates.create! }
     elsif diff < 0
       self.audio_file_templates = audio_file_templates[0, segment_count]
     end
--- test/models/audio_version_template_test.rb
+++ test/models/audio_version_template_test.rb
@@ -11,4 +11,24 @@ describe AudioVersionTemplate do
   it 'has a series' do
     audio_version_template.series.wont_be_nil
   end
+
+  it 'can be created with valid attributes' do
+    audio_version_template.must_be :valid?
+  end
+
+  it 'reduce file templates when segment count lowered' do
+    audio_version_template.segment_count.must_equal 4
+    audio_version_template.audio_file_templates.count.must_equal 4
+    audio_version_template.segment_count = 2
+    audio_version_template.save
+    audio_version_template.audio_file_templates(true).count.must_equal 2
+  end
+
+  it 'add file templates when segment count raised' do
+    audio_version_template.segment_count.must_equal 4
+    audio_version_template.audio_file_templates.count.must_equal 4
+    audio_version_template.segment_count = 6
+    audio_version_template.save
+    audio_version_template.audio_file_templates(true).count.must_equal 6
+  end
 end
