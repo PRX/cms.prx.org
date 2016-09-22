a9d5966fdc4af3626df597407b8eb5645a3901fd
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-09-22 11:28:12 -0400

    network test

---
+++ test/models/network_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+describe Network do
+  let(:network) { create(:network) }
+
+  it 'has a table defined' do
+    Network.table_name.must_equal 'networks'
+  end
+
+  it 'can create a valid network membership' do
+    network.account.wont_be_nil
+  end
+
+  it 'returns a list of stories' do
+    network.stories.count.must_equal 2
+    network.all_stories.count.must_equal 2
+  end
+end
