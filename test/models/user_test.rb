ae9e57db075703e637edcc6585e20e8ceeb56650
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-09-27 17:05:13 -0400

    no all_stoires for account

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -40,7 +40,7 @@ describe User do
   end
 
   it 'has a list of stories for approved accounts' do
-    start_count = user.individual_account.all_stories.count
+    start_count = user.individual_account.stories.count
     create(:account, user: user, stories_count: 2)
     user.approved_account_stories.count.must_equal start_count + 2
   end
