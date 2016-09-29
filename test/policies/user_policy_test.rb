1a1a63c8dd39fe60316251f7d6627cdc889c64ad
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-09-29 09:02:32 -0400

    Change user policy to prevent create, this is an id.orx.org responsibility

--- test/policies/user_policy_test.rb
+++ test/policies/user_policy_test.rb
@@ -5,13 +5,20 @@ describe UserPolicy do
   let(:other_token) { StubToken.new(123, ['member']) }
   let(:user) { build_stubbed(:user, id: user_token.user_id) }
 
-  describe '#update?' do
+  describe '#update? and #create?' do
     it 'lets users update themselves' do
       UserPolicy.new(user_token, user).must_allow :update?
     end
 
     it 'does not let users update each other' do
       UserPolicy.new(other_token, user).wont_allow :update?
+    end
+  end
+
+  describe '#create?' do
+    it 'never allows create' do
+      UserPolicy.new(user_token, user).wont_allow :create?
+      UserPolicy.new(other_token, user).wont_allow :create?
     end
   end
 end
