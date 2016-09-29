7c35e70d58c013a7dfe0ae9d7c6b9f0528d92126
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-09-28 22:15:09 -0400

    Pass in parent to image for path helpers, except for story

--- app/representers/api/image_representer.rb
+++ app/representers/api/image_representer.rb
@@ -5,9 +5,9 @@ class Api::ImageRepresenter < Api::BaseRepresenter
     if represented.is_a?(StoryImage)
       api_story_story_image_path(represented.story, represented)
     elsif represented.is_a?(SeriesImage)
-      api_series_series_image_path(represented.series, represented)
+      api_series_series_image_path(represented.series)
     elsif represented.is_a?(AccountImage)
-      api_account_account_image_path(represented.account, represented)
+      api_account_account_image_path(represented.account)
     elsif represented.is_a?(UserImage)
       api_user_user_image_path(represented.user)
     else
--- app/representers/api/msg/image_representer.rb
+++ app/representers/api/msg/image_representer.rb
@@ -4,6 +4,20 @@
 # Representer for Announce messaging
 #
 class Api::Msg::ImageRepresenter < Api::BaseRepresenter
+  def self_url(represented)
+    if represented.is_a?(StoryImage)
+      api_story_story_image_path(represented.story, represented)
+    elsif represented.is_a?(SeriesImage)
+      api_series_series_image_path(represented.series)
+    elsif represented.is_a?(AccountImage)
+      api_account_account_image_path(represented.account)
+    elsif represented.is_a?(UserImage)
+      api_user_user_image_path(represented.user)
+    else
+      super
+    end
+  end
+
   property :id, writeable: false
   property :filename, writeable: false
   property :size
