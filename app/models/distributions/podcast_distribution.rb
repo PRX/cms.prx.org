1efde2a9f17782533ba17718c0721d82dea8edc8
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-11-29 17:50:25 -0500

    Create feeder podcast for a series podcast distribution

--- app/models/distributions/podcast_distribution.rb
+++ app/models/distributions/podcast_distribution.rb
@@ -1,4 +1,24 @@
+require 'prx_access'
+
 module Distributions
-  class PodcastDistribution < Distribution
+  class PodcastDistribution < ::Distribution
+    include PRXAccess
+    include Rails.application.routes.url_helpers
+
+    # after_commit :add_podcast_to_feeder, on: [:create]
+
+    def add_podcast_to_feeder
+      client = api(root: feeder_root, account: account.id)
+      podcast = client.podcasts.first.post(podcast_attributes)
+      podcast_url = URI.join(feeder_root, podcast.links['self'].href).to_s
+      self.update_attribute(:url, podcast_url) if podcast_url
+    end
+
+    def podcast_attributes
+      {
+        prx_uri: polymorphic_url(['api', owner], only_path: true),
+        prx_account_uri: api_account_path(account)
+      }
+    end
   end
 end
