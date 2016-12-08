8def5a1e33eaf79b47a657dcc526dd02c9cd41bc
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-12-08 12:05:53 -0500

    add story distro nested controller

---
+++ app/controllers/api/story_distributions_controller.rb
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+class Api::StoryDistributionsController < Api::BaseController
+  api_versions :v1
+
+  filter_resources_by :story_id
+
+  announce_actions resource: :story_resource
+
+  def story_resource
+    resource.try(:story)
+  end
+
+  def create
+    create_resource.tap do |res|
+      consume! res, create_options
+      res = res.becomes(res.type.safe_constantize) if res.type
+      hal_authorize res
+      res.save!
+      distribute(res)
+      respond_with root_resource(res), create_distribution_options(res)
+    end
+  end
+
+  def create_distribution_options(distribution)
+    create_options.tap do |options|
+      dec = decorator_for_model(distribution)
+      options[:represent_with] = dec if dec
+    end
+  end
+
+  def distribute(distribution)
+    distribution.distribute
+  end
+end
---
+++ test/controllers/api/story_distribitions_controller_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+describe Api::StoryDistributionsController do
+
+  let(:story_distribution) { create(:podcast_episode_distribution) }
+  let(:distribution) { story_distribution.distribution }
+  let(:story) { story_distribution.story }
+  let(:token) { StubToken.new(account.id, ['member']) }
+
+  it 'should show' do
+    get :show, api_request_opts(story_id: story.id, id: story_distribution.id)
+    assert_response :success
+  end
+
+  # before(:each) do
+  #   class << @controller; attr_accessor :prx_auth_token; end
+  #   @controller.prx_auth_token = token
+  # end
+  #
+  # describe '#create' do
+  #   it 'can create a podcast distribution for a series' do
+  #     pd_hash = {
+  #       kind: 'podcast'
+  #     }
+  #     @request.env['CONTENT_TYPE'] = 'application/json'
+  #     def @controller.distribute(res)
+  #       res.update_column(:url, 'http://feeder.prx.org/api/v1/podcast/12345')
+  #     end
+  #     post :create, pd_hash.to_json, api_request_opts(series_id: series.id)
+  #     assert_response :success
+  #     resource = JSON.parse(response.body)
+  #     resource['_links']['prx:podcast']['href'].must_equal 'http://feeder.prx.org/api/v1/podcast/12345'
+  #   end
+  # end
+end
