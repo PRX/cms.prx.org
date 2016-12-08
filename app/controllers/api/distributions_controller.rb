224dbb8221b3a9ff01a64472cd15f2569af6a8bf
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-12-08 11:55:44 -0500

    create a podcast distribution posting to the distributions endpoint

--- app/controllers/api/distributions_controller.rb
+++ app/controllers/api/distributions_controller.rb
@@ -14,4 +14,26 @@ class Api::DistributionsController < Api::BaseController
   def filtered(arel)
     polymorphic_filtered('distributable', arel)
   end
+
+  def create
+    create_resource.tap do |res|
+      consume! res, create_options
+      res = res.becomes(res.type.safe_constantize) if res.type
+      hal_authorize res
+      res.save!
+      distribute(res)
+      respond_with root_resource(res), create_distribution_options(res)
+    end
+  end
+
+  def create_distribution_options(distribution)
+    create_options.tap do |options|
+      dec = decorator_for_model(distribution)
+      options[:represent_with] = dec if dec
+    end
+  end
+
+  def distribute(distribution)
+    distribution.distribute
+  end
 end
