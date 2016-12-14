f69e0922cfc94c9938ba84ce33ba3d6df33023ab
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-12-14 13:35:55 -0500

    houndly

--- test/factories/distribution_factory.rb
+++ test/factories/distribution_factory.rb
@@ -3,13 +3,13 @@ FactoryGirl.define do
     distributable factory: :series
     url 'https://feeder.prx.org/api/v1/podcasts/23'
     guid 'testpodcast'
-    properties( { 'explicit' => 'clean' } )
+    properties({ 'explicit' => 'clean' })
   end
 
   factory :podcast_distribution, class: 'Distributions::PodcastDistribution' do
     distributable factory: :series
     url 'https://feeder.prx.org/api/v1/podcasts/23'
     guid 'testpodcast'
-    properties( { 'explicit' => 'clean' } )
+    properties({ 'explicit' => 'clean' })
   end
 end
