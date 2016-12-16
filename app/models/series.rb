0bcc1b0bcc605802eb074bd03fb833255f641f83
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-12-16 17:39:53 -0500

    update series to have a default image and multiple images

--- app/models/series.rb
+++ app/models/series.rb
@@ -20,7 +20,8 @@ class Series < BaseModel
   has_many :audio_version_templates
   has_many :distributions, as: :distributable
 
-  has_one :image, -> { where(parent_id: nil) }, class_name: 'SeriesImage', dependent: :destroy
+  # has_one :image, -> { where(parent_id: nil) }, class_name: 'SeriesImage', dependent: :destroy
+  has_many :images, -> { where(parent_id: nil) }, class_name: 'SeriesImage', dependent: :destroy
 
   before_validation :set_app_version, on: :create
 
@@ -33,6 +34,10 @@ class Series < BaseModel
           "`series`.`short_description` like '%#{text}%' OR " +
           "`series`.`description` like '%#{text}%'")
   }
+
+  def default_image
+    @default_image ||= images.first
+  end
 
   def subscribable?
     subscription_approval_status == SUBSCRIPTION_PRX_APPROVED
