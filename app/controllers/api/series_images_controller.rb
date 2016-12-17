757d0e189ca2814e076c8ae40a9ed2eeb7220361
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-12-17 08:56:58 -0500

    no longer a child resource

--- app/controllers/api/series_images_controller.rb
+++ app/controllers/api/series_images_controller.rb
@@ -1,8 +1,6 @@
 # encoding: utf-8
 
 class Api::SeriesImagesController < Api::BaseController
-  include Announce::Publisher
-
   api_versions :v1
 
   filter_resources_by :series_id
@@ -10,10 +8,4 @@ class Api::SeriesImagesController < Api::BaseController
   announce_actions decorator: Api::Msg::ImageRepresenter, subject: :image
 
   represent_with Api::ImageRepresenter
-
-  child_resource parent: 'series', child: 'image'
-
-  def after_original_destroyed(original)
-    announce('image', 'destroy', Api::Msg::ImageRepresenter.new(original).to_json)
-  end
 end
