6c94e9d31e55ca1a35b5fd2b469b46cc020ea5a6
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-12-19 09:40:34 -0500

    include all the images

--- app/representers/api/series_representer.rb
+++ app/representers/api/series_representer.rb
@@ -38,7 +38,7 @@ class Api::SeriesRepresenter < Api::BaseRepresenter
       count: represented.images.count
     } if represented.id
   end
-  embed :images, paged: true, item_class: SeriesImage, decorator: Api::ImageRepresenter, zoom: false
+  embed :images, paged: true, item_class: SeriesImage, decorator: Api::ImageRepresenter
 
   link :account do
     {
