897cefd352f0c62275ba90790e4ca194761d7b43
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-12-20 17:55:51 -0500

    happy hound

--- test/representers/api/distributions/podcast_distribution_representer_test.rb
+++ test/representers/api/distributions/podcast_distribution_representer_test.rb
@@ -3,19 +3,19 @@ require 'test_helper'
 describe Api::Distributions::PodcastDistributionRepresenter do
 
   let(:series) { create(:series) }
-  let(:audio_version_template) { create(:audio_version_template, series: series) }
-  let(:distribution) { create(:podcast_distribution, audio_version_template: audio_version_template) }
+  let(:template) { create(:audio_version_template, series: series) }
+  let(:distribution) { create(:podcast_distribution, audio_version_template: template) }
   let(:representer) { Api::Distributions::PodcastDistributionRepresenter.new(distribution) }
   let(:json) { JSON.parse(representer.to_json) }
 
   before do
     stub_request(:get, "http://feeder.docker/api/v1").
-      with(:headers => {'Accept'=>'application/json', 'Authorization'=>'Bearer token', 'Content-Type'=>'application/json'}).
-      to_return(:status => 200, :body => json_file('feeder_root'))
+      with(headers: { 'Authorization'=>'Bearer token', 'Content-Type'=>'application/json' }).
+      to_return(status: 200, body: json_file('feeder_root'))
 
     stub_request(:post, "http://feeder.docker/api/v1/podcasts").
-      with(:headers => {'Authorization'=>'Bearer token', 'Content-Type'=>'application/json'}).
-      to_return(:status => 200, :body => json_file('podcast'), :headers => {})
+      with(headers: {'Authorization'=>'Bearer token', 'Content-Type'=>'application/json'}).
+      to_return(status: 200, body: json_file('podcast'))
   end
 
   it 'create representer' do
@@ -28,7 +28,7 @@ describe Api::Distributions::PodcastDistributionRepresenter do
   end
 
   it 'should have a link to an audio version template' do
-    template_path = "/api/v1/audio_version_templates/#{audio_version_template.id}"
+    template_path = "/api/v1/audio_version_templates/#{template.id}"
     json['_links']['prx:audio-version-template']['href'].must_equal template_path
   end
 end
