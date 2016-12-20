53f17570f5e4aa832bd779d5d5fcdec6c2ea8e67
Andrew Kuklewicz <andrew@beginsinwonder.com>
2016-12-20 14:45:19 -0500

    raise exception when podcast create fails or podcast url cannot be set

--- test/models/distributions/podcast_distribution_test.rb
+++ test/models/distributions/podcast_distribution_test.rb
@@ -7,12 +7,16 @@ describe Distributions::PodcastDistribution do
 
   before do
     stub_request(:get, 'https://feeder.prx.org/api/v1').
-      with(headers: { 'Authorization' => 'Bearer token', 'Content-Type' => 'application/json' }).
+      with(headers: { 'Content-Type' => 'application/json' }).
       to_return(status: 200, body: json_file('feeder_root'))
 
     stub_request(:post, 'https://feeder.prx.org/api/v1/podcasts').
       with(headers: { 'Authorization' => 'Bearer token', 'Content-Type' => 'application/json' }).
       to_return(status: 200, body: json_file('podcast'), headers: {})
+
+      stub_request(:post, 'https://feeder.prx.org/api/v1/podcasts').
+        with(headers: { 'Authorization' => 'Bearer fail', 'Content-Type' => 'application/json' }).
+        to_return(status: 500, body: "{'code':'500', 'message':'dupe'}", headers: {})
 
     stub_request(:get, 'https://feeder.prx.org/api/v1/podcasts/23').
       with(headers: { 'Authorization' => 'Bearer token', 'Content-Type' => 'application/json' }).
@@ -20,6 +24,17 @@ describe Distributions::PodcastDistribution do
   end
 
   it 'creates the podcast on feeder' do
+    distribution.stub(:get_account_token, 'fail') do
+      distribution.url = nil
+      distribution.account.wont_be_nil
+      distribution.save!
+      -> do
+        distribution.distribute!
+      end.must_raise(HyperResource::ServerError)
+    end
+  end
+
+  it 'throws error and rolls back if podcast on feeder fails' do
     distribution.stub(:get_account_token, 'token') do
       distribution.url = nil
       distribution.account.wont_be_nil
