9e1faa5bfa0dd6c51aed4d936c2640be5f4aef26
Andrew Kuklewicz <andrew@beginsinwonder.com>
2017-01-12 12:57:17 -0500

    add new callbacks for controllers for after resource save, before response created

---
+++ app/controllers/concerns/resource_callbacks.rb
@@ -0,0 +1,45 @@
+# encoding: utf-8
+
+require 'active_support/concern'
+
+module ResourceCallbacks
+  extend ActiveSupport::Concern
+
+  def create
+    create_resource.tap do |res|
+      consume! res, create_options
+      hal_authorize res
+      res.save!
+      after_create_resource(res)
+      respond_with root_resource(res), create_options
+    end
+  end
+
+  def update
+    update_resource.tap do |res|
+      consume! res, show_options
+      hal_authorize res
+      res.save!
+      after_update_resource(res)
+      respond_with root_resource(res), show_options
+    end
+  end
+
+  def destroy
+    destroy_resource.tap do |res|
+      hal_authorize res
+      res.destroy
+      after_destroy_resource(res)
+      head :no_content
+    end
+  end
+
+  def after_create_resource(_resource = nil)
+  end
+
+  def after_update_resource(_resource = nil)
+  end
+
+  def after_destroy_resource(_resource = nil)
+  end
+end
