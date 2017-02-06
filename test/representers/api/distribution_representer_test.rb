c1205f552bfc9b7af109949c6bb6d21f94b93441
gcampo88 <gigi.campo@gmail.com>
2017-02-06 11:27:34 -0500

    Nil out audio_version_template_id if template is destroyed

--- test/representers/api/distribution_representer_test.rb
+++ test/representers/api/distribution_representer_test.rb
@@ -4,8 +4,8 @@ require 'test_helper'
 require 'distribution' if !defined?(Distribution)
 
 describe Api::DistributionRepresenter do
-
-  let(:distribution) { FactoryGirl.create(:distribution) }
+  let(:audio_version_template) { create(:audio_version_template) }
+  let(:distribution) { FactoryGirl.create(:distribution, audio_version_template: audio_version_template) }
   let(:representer)   { Api::DistributionRepresenter.new(distribution) }
   let(:json)          { JSON.parse(representer.to_json) }
 
@@ -16,5 +16,11 @@ describe Api::DistributionRepresenter do
   it 'use representer to create json' do
     json['id'].must_equal distribution.id
     json['properties']['explicit'].must_equal 'clean'
+  end
+
+  it 'doesnt link to the template if the template has been destroyed' do
+    representer.represented.audio_version_template.destroy!
+    representer.represented.reload
+    json['_links']['prx:audio-version-template'].must_be_nil
   end
 end
