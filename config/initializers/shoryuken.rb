22a8164f6a92cd1ad758da015eecae2082f66f5e
Andrew Kuklewicz <andrew@beginsinwonder.com>
2017-02-14 15:18:49 -0500

    add in a job to process import

--- config/initializers/shoryuken.rb
+++ config/initializers/shoryuken.rb
@@ -1,8 +1,10 @@
 require 'shoryuken'
+require 'shoryuken/extensions/active_job_adapter'
 
 Shoryuken.default_worker_options = {
+  'queue'                   => "#{Rails.env}_cms_default",
   'auto_delete'             => true,
-  'auto_visibility_timeout' => false,
+  'auto_visibility_timeout' => true,
   'batch'                   => false,
   'body_parser'             => :json
 }
