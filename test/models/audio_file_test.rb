1f0048e159374895bd0e6600e5b7b27067d8daa4
gcampo88 <gigi.campo@gmail.com>
2017-02-15 12:21:26 -0500

    Use FactoryGirl over mocks

--- test/models/audio_file_test.rb
+++ test/models/audio_file_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 
 describe AudioFile do
-
-  let(:audio_file) { FactoryGirl.create(:audio_file) }
-
+  let(:audio_version_template_with_file_templates) {
+    FactoryGirl.create(:audio_version_template_with_file_templates)
+  }
+  let(:audio_version) { FactoryGirl.create(:audio_version_with_template,
+    audio_version_template: audio_version_template_with_file_templates)}
+  let(:audio_file) { FactoryGirl.create(:audio_file, audio_version: audio_version) }
   let(:audio_file_uploaded) { FactoryGirl.create(:audio_file_uploaded) }
 
   it 'has a table defined' do
@@ -46,17 +49,11 @@ describe AudioFile do
   end
 
   it 'validates self based on template' do
-    mock_version = MiniTest::Mock.new
-    mock_version_template = MiniTest::Mock.new
-    mock_templates_list = MiniTest::Mock.new
-    mock_file_template = MiniTest::Mock.new
-
-    mock_version.expect(:audio_version_template, mock_version_template)
-    mock_version_template.expect(:audio_file_templates, mock_templates_list)
-    mock_templates_list.expect(:find, mock_file_template)
-
-    audio_file.stub(:audio_version, mock_version) do
-      audio_file.update_attributes!(label: "Main Segment", position: 1)
+    aft = audio_version_template_with_file_templates.audio_file_templates.find do |ft|
+      ft.label == "Main Segment" && ft.position == 1
     end
+    aft.stub(:validate_audio_file, audio_file) { true }
+    audio_file.update_attributes!(label: "Main Segment", position: 1)
+    audio_file.audio_status.must_equal "Valid"
   end
 end
