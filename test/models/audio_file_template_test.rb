757ade63f915598f4379282d90c061b3561f35dc
gcampo88 <gigi.campo@gmail.com>
2017-02-16 11:44:11 -0500

    Add a test for audio file template

--- test/models/audio_file_template_test.rb
+++ test/models/audio_file_template_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 describe AudioFileTemplate do
 
   let(:audio_file_template) { create(:audio_file_template) }
+  let(:audio_file) { create(:audio_file, label: 'Main Segment', position: 1) }
 
   it 'has a table defined' do
     AudioFileTemplate.table_name.must_equal 'audio_file_templates'
@@ -10,5 +11,9 @@ describe AudioFileTemplate do
 
   it 'has a version template' do
     audio_file_template.audio_version_template.wont_be_nil
+  end
+
+  it 'can validate a file against itself' do
+    audio_file_template.validate_audio_file_lengths(audio_file).must_include 'must be between'
   end
 end
