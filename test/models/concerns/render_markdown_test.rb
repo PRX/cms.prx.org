7f0ccb91812098d8c7a32bf7d40c63702c949b83
Andrew Kuklewicz <andrew@beginsinwonder.com>
2017-06-14 15:37:51 -0400

    handle escaped newlines

--- test/models/concerns/render_markdown_test.rb
+++ test/models/concerns/render_markdown_test.rb
@@ -17,4 +17,10 @@ describe TestMarkdownModel do
   it 'converts html to markdown ' do
     model.html_to_markdown("\n<p><em>This is markdown</em></p>\n").must_equal "_This is markdown_"
   end
+
+  it 'converts escaped new lines without slashes' do
+    model.markdown_to_html("one\n\n\\\nanother\n").must_equal "<p>one</p>\n\n<p><br>\nanother</p>"
+    model.markdown_to_html("one\n\\\n\\\n\\\nanother\n").must_equal "<p>one<br>\n<br>\n<br>\n<br>\nanother</p>"
+    model.markdown_to_html("one\n\\\n\\\nanother\n").must_equal "<p>one<br>\n<br>\n<br>\nanother</p>"
+  end
 end
