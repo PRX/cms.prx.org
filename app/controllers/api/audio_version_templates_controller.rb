9a676489f185f81d974b99a9775337d2b688859d
Andrew Kuklewicz <andrew@beginsinwonder.com>
2017-09-19 12:54:43 -0400

    fix line length

--- app/controllers/api/audio_version_templates_controller.rb
+++ app/controllers/api/audio_version_templates_controller.rb
@@ -10,7 +10,8 @@ class Api::AudioVersionTemplatesController < Api::BaseController
   def filtered(arel)
     if params['distribution_id']
       distribution_id = params.delete('distribution_id')
-      arel = arel.joins(:distribution_templates).where('distribution_templates.distribution_id = ?', distribution_id)
+      arel = arel.joins(:distribution_templates)
+      arel = arel.where('distribution_templates.distribution_id = ?', distribution_id)
     end
     super(arel)
   end
