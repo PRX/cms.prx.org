55642798a0e7f86b8ae6b11a83c036596c3e84aa
Andrew Kuklewicz <andrew@beginsinwonder.com>
2017-09-18 20:38:20 -0400

    set template for the same series

--- test/models/podcast_import_test.rb
+++ test/models/podcast_import_test.rb
@@ -17,9 +17,9 @@ describe PodcastImport do
   let(:series) { create(:series) }
   let(:template) { create(:audio_version_template, series: series) }
   let(:distribution) do
-    build(:podcast_distribution,
-          audio_version_template: template,
-          url: 'https://feeder.prx.org/api/v1/podcasts/51')
+    create(:podcast_distribution, distributable: series, url: 'https://feeder.prx.org/api/v1/podcasts/51').tap do |dist|
+      dist.audio_version_templates << template
+    end
   end
 
   let(:podcast) do
