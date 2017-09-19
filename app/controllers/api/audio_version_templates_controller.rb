91363b02b620d679c2538fb806619a70719ad630
Andrew Kuklewicz <andrew@beginsinwonder.com>
2017-09-19 12:52:17 -0400

    filter templates by distribution

--- app/controllers/api/audio_version_templates_controller.rb
+++ app/controllers/api/audio_version_templates_controller.rb
@@ -3,7 +3,15 @@
 class Api::AudioVersionTemplatesController < Api::BaseController
   api_versions :v1
 
-  filter_resources_by :series_id
+  filter_resources_by :series_id, :distribution_id
 
   sort_params default: { label: :asc }, allowed: [:id, :label]
+
+  def filtered(arel)
+    if params['distribution_id']
+      distribution_id = params.delete('distribution_id')
+      arel = arel.joins(:distribution_templates).where('distribution_templates.distribution_id = ?', distribution_id)
+    end
+    super(arel)
+  end
 end
