4e30f2816ef942181a8a6735ce71b4f1851fc9a0
Andrew Kuklewicz <andrew@beginsinwonder.com>
2017-10-26 18:14:00 -0400

    moved to gem

--- app/models/base_model.rb
+++ app/models/base_model.rb
@@ -5,10 +5,6 @@ class BaseModel < ActiveRecord::Base
 
   include RepresentedModel
 
-  def id_from_url(url)
-    Rails.application.routes.recognize_path(url)[:id]
-  end
-
   def update_file!(name)
     filename_will_change!
     raw_write_attribute(:filename, name)
