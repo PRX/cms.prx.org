d156901c90d46000fcb13579394eadb39ac5ac13
Peter Karman <peter.karman@prx.org>
2018-08-31 10:10:48 -0500

    Switch anon search to public_filter
    The ES boolean filter logic now attempts to mimic the SQL filters
    like:
    
    WHERE (
            `pieces`.`deleted_at` IS NULL
            OR `pieces`.`app_version` = 'v4'
            )
        AND (`published_at` <= NOW())
        AND (`network_only_at` IS NULL)
        AND (
            `series`.`subscription_approval_status` != 'PRX Approved'
            OR `series`.`subscriber_only_at` IS NULL
            )
    
    See relevant docs including
    https://www.elastic.co/guide/en/elasticsearch/guide/master/combining-filters.html
    
    We add the _name attribute to each filter to make it easier to see why a particular
    hit was filtered. With the ES_DEBUG=1 env var, the logs look like:
    
    cms_1            |    #       {
    cms_1            |    #         "_index":"stories",
    cms_1            |    #         "_type":"story",
    cms_1            |    #         "_id":"190047",
    cms_1            |    #         "_score":null,
    cms_1            |    #         "_source":{
    cms_1            |    #           "id":190047
    cms_1            |    #         },
    cms_1            |    #         "sort":[
    cms_1            |    #           1520377089000,
    cms_1            |    #           1520377089000
    cms_1            |    #         ],
    cms_1            |    #         "matched_queries":[
    cms_1            |    #           "deleted_at_null",
    cms_1            |    #           "app_version_v4",
    cms_1            |    #           "published"
    cms_1            |    #         ]
    cms_1            |    #       },

--- app/services/story_query_builder.rb
+++ app/services/story_query_builder.rb
@@ -17,6 +17,10 @@ class StoryQueryBuilder < ESQueryBuilder
   end
 
   def apply_published_filter?
+    false # TODO
+  end
+
+  def apply_public_filter?
     !apply_authz?
   end
 
@@ -30,20 +34,74 @@ class StoryQueryBuilder < ESQueryBuilder
     if apply_authz?
       bools.push authz_filter
     end
+    if apply_public_filter?
+      bools.push published_filter
+      bools.push v4_or_deleted_at_null_filter
+      bools.push network_visible_filter
+      bools.push series_visible_filter
+    end
     bools
   end
 
   def published_filter
     searchdsl = self
     Filter.new do
-      range published_at: { lte: 'now' }
+      range published_at: { lte: 'now', _name: :published }
     end
   end
 
   def authz_filter
     searchdsl = self
     Filter.new do
-      terms account_id: searchdsl.authorization.token_auth_accounts.try(:ids)
+      terms account_id: { value: searchdsl.authorization.token_auth_accounts.try(:ids), _name: :authz }
+    end
+  end
+
+  def v4_or_deleted_at_null_filter
+    Filter.new do
+      bool do
+        should do
+          bool do
+            must_not do
+              exists field: :deleted_at, _name: :deleted_at_null
+            end
+          end
+        end
+        should do
+          term app_version: { value: 'v4', _name: :app_version_v4 }
+        end
+      end
+    end
+  end
+
+  def network_visible_filter
+    Filter.new do
+      bool do
+        must_not do
+          exists field: :network_only_at, _name: :network_visible
+        end
+      end
+    end
+  end
+
+  def series_visible_filter
+    Filter.new do
+      bool do
+        should do
+          bool do
+            must_not do
+              term 'series.subscription_approval_status' => { value: 'PRX Approved', _name: :prx_series_approved }
+            end
+          end
+        end
+        should do
+          bool do
+            must_not do
+              exists field: 'series.subscriber_only_at', _name: :series_subscriber_only_at
+            end
+          end
+        end
+      end
     end
   end
 end
