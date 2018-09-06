ea7d246648065ae7f0febf494685ca71bc579986
cavis <ryancavis@gmail.com>
2018-09-06 09:51:51 -0600

    Allow running tests with no ES

--- test/support/elasticsearch_helper.rb
+++ test/support/elasticsearch_helper.rb
@@ -48,5 +48,7 @@ class ElasticsearchHelper
     [User, Series, Story, StationAccount, GroupAccount, Playlist].each do |klass|
       helper.create_es_index(klass)
     end
+  rescue Faraday::ConnectionFailed => e
+    puts "WARN: No elasticsearch connection"
   end
 end
