de421170cc620fa0b4897f0beff65e9869f7c0c1
Peter St.Jean <peter@stjean.co>
2018-12-14 15:57:31 -0600

    Typo :man-facepalming:

--- app/policies/individual_account_policy.rb
+++ app/policies/individual_account_policy.rb
@@ -1,8 +1,9 @@
 class IndividualAccountPolicy < AccountPolicy
   def create?
+    binding.pry
     # An account without a membership to an Individual Account
     token.present? && !IndividualAccount.
       joins(:memberships).
-      find_by('memberships.user_id', token.user_id)
+      find_by('memberships.user_id': token.user_id)
   end
 end
