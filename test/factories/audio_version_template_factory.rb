bf76f5f0de6c8976b52830c4c68b1a1e2e99686e
cavis <ryancavis@gmail.com>
2019-01-29 14:09:53 -0700

    Avoid some factory recursion

--- test/factories/audio_version_template_factory.rb
+++ test/factories/audio_version_template_factory.rb
@@ -1,10 +1,19 @@
 FactoryGirl.define do
   factory :audio_version_template do
-    series
+
+    transient do
+      series { false }
+    end
+
     label 'Audio Version'
     content_type 'audio/mpeg'
     length_minimum 10
     length_maximum 100
+
+    after(:create) do |avt, evaluator|
+      avt.series = create(:series, templates_count: 0) if evaluator.series
+    end
+
     factory :audio_version_template_with_file_templates do
       after(:create) do |avt|
         create_list(:audio_file_template, 3, audio_version_template: avt)
