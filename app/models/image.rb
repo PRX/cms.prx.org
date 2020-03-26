4aad93f7b4c559b68bb4d1baf97ddb1ef97f24ee
Chris Quamme Rhoden <carhoden@gmail.com>
2020-03-26 14:33:28 -0400

    Resize images with appropriate cropping & letterboxing

--- app/models/image.rb
+++ app/models/image.rb
@@ -159,7 +159,7 @@ class Image < BaseModel
         Format: format,
         Metadata: 'PRESERVE',
         Resize: {
-          Fit: 'contain',
+          Fit: name == 'square' ? 'cover' : 'inside',
           Height: dimensions[1],
           Position: 'centre',
           Width: dimensions[0]
