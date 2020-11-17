64ee7f75fc5a12154b7cd2988f325eca8f55cf3e
Chris Kalafarski <chris@farski.com>
2020-11-17 10:03:36 -0500

    Update schema version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200304162904) do
+ActiveRecord::Schema.define(version: 20201102163043) do
 
   create_table "account_applications", force: :cascade do |t|
     t.integer  "account_id",            limit: 4
