9bce428a5e213601d78d39eeb601d0fb474f4dbb
Chris Kalafarski <chris@farski.com>
2021-12-01 14:01:14 -0500

    Use IAM profile when key isn't available

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,13 +3,23 @@ CarrierWave.configure do |config|
   config.fog_directory   = ENV['AWS_BUCKET']
   config.fog_public      = false
   config.fog_attributes  = {}
-  config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region:                ENV['AWS_REGION'] || 'us-east-1',
-    path_style:            true
-  }
+
+  if (ENV['AWS_ACCESS_KEY_ID'] && ENV['AWS_SECRET_ACCESS_KEY'])
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region:                ENV['AWS_REGION'] || 'us-east-1',
+      path_style:            true
+    }
+  else
+    config.fog_credentials = {
+      provider:              'AWS',
+      use_iam_profile:       true,
+      region:                ENV['AWS_REGION'] || 'us-east-1',
+      path_style:            true
+    }
+  end
 
   config.remove_previously_stored_files_after_update = false
 end
